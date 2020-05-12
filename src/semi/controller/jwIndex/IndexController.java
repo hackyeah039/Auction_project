8312698ba670a531f44e4b591b58e1c6bdb87df4
hackyeah039 <jwjs3219@gmail.com>
2020-05-12 15:33:49 +0900

    pull전 commit

--- src/semi/controller/jwIndex/IndexController.java
+++ src/semi/controller/jwIndex/IndexController.java
@@ -16,7 +16,6 @@ public class IndexController extends HttpServlet{
 			throws ServletException, IOException {
 		//detailView에서 경매상품에 대한 num를 req.getparameter()로 받아오기
 		AuctionDao dao = new AuctionDao();
-		dao.enddate(num);
 		
 	}
 }
