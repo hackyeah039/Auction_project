8fdb696191ef584e45c20c99e8c8130cef1e12e2
admin <admin@DESKTOP-J54BCRR>
2020-05-14 00:00:06 +0900

    auctiondao - 경매번호로 vo리턴 메소드

--- src/semi/dao/yh/AuctionDao.java
+++ src/semi/dao/yh/AuctionDao.java
@@ -1,5 +1,11 @@
 package semi.dao.yh;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import jdbc.JdbcUtil;
 import semi.vo.yh.AuctionVo;
 
 public class AuctionDao {
@@ -10,7 +16,40 @@ public class AuctionDao {
 	public static AuctionDao getInstance() {
 		return instance;
 	}
-	public int insert(AuctionVo vo) {
-		
+	public AuctionVo searchAuction(int a_num) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = JdbcUtil.getConn();
+			String sql = "select * from auction where a_num = ?";
+			pstmt.setInt(1, a_num);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				AuctionVo vo = new AuctionVo(
+						rs.getInt("a_num"),
+						rs.getString("a_title"),
+						rs.getString("a_content"),
+						rs.getString("a_condition"),
+						rs.getDate("a_regdate"),
+						rs.getString("a_startdate"),
+						rs.getString("a_enddate"),
+						rs.getInt("a_check"),
+						rs.getInt("c_num"),
+						rs.getInt("a_jjim"),
+						rs.getInt("sel_number"),
+						rs.getInt("bidstatus"),
+						rs.getInt("a_startbid"),
+						rs.getInt("a_bidunit")
+						);
+				return vo;
+			}
+			return null;
+		} catch (SQLException se) {
+			se.printStackTrace();
+			return null;
+		} finally {
+			JdbcUtil.close(con, pstmt, rs);
+		}
 	}
 }
