37c6fd47576fb17661d4ce437f6b06e2fbf3bbbe
JHTA <JHTA@JHTA-PC>
2020-05-15 17:42:27 +0900

    테이블 인서트 메소드 추가

--- src/semi/dao/yh/AuctionDao.java
+++ src/semi/dao/yh/AuctionDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import jdbc.JdbcUtil;
 import semi.vo.yh.AuctionVo;
@@ -32,7 +33,7 @@ public class AuctionDao {
 						rs.getInt("a_num"),
 						rs.getString("a_title"),
 						rs.getString("a_content"),
-						rs.getString("a_condition"),
+						rs.getInt("a_condition"),
 						rs.getDate("a_regdate"),
 						rs.getString("a_startdate"),
 						rs.getString("a_enddate"),
@@ -52,6 +53,33 @@ public class AuctionDao {
 			return null;
 		} finally {
 			JdbcUtil.close(con, pstmt, rs);
+		}
+	}
+	//테이블 인서트 메소드 추가 
+	public int InsertAuction(AuctionVo vo) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int n = 0;
+		try {
+			con = JdbcUtil.getConn();
+			String sql = "insert into auction values(seq_auction_a_num.nextval,?,?,?,sysdate,?,?,0,?,0,?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, vo.getA_title());
+			pstmt.setString(2, vo.getA_content());
+			pstmt.setInt(3, vo.getA_condition());
+			pstmt.setString(4, vo.getA_startdate());
+			pstmt.setString(5, vo.getA_enddate());
+			pstmt.setInt(6, vo.getC_num());
+			pstmt.setInt(7, vo.getSel_number());
+			pstmt.setInt(8, vo.getBidstatus());
+			pstmt.setInt(9, vo.getA_startbid());
+			pstmt.setInt(10, vo.getA_bidunit());
+			return n = pstmt.executeUpdate();	
+		} catch (SQLException se) {
+			se.printStackTrace();
+			return -1;
+		} finally {
+			JdbcUtil.close(con, pstmt, null);
 		}
 	}
 }
