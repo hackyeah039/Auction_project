ea2ce49d0d4b67585baf78fa638d7f5f5809da23
admin <admin@DESKTOP-J54BCRR>
2020-05-16 17:11:17 +0900

    0516 - 처음 호출시 불러올 홈페이지 주소 값 넣기 완료

--- src/semi/controller/yhauction/HomeController.java
+++ src/semi/controller/yhauction/HomeController.java
@@ -13,12 +13,23 @@ public class HomeController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+/*		// 홈페이지 호출시 기본적으로 보여질 페이지 설정.
+		String top = (String)req.getAttribute("header");
+		String content = (String)req.getAttribute("content");
+		// 초기에는 호출된 값들이 없으므로 null 값일때 보여질 주소값 대입
+		if(top == null) top = "/header.jsp";
+		if(content == null) top = "/mainpage.jsp";
+*/		
+/*
+		//페이지 이동 전 페이지 주소에 대한 값을 담음
+		req.setAttribute("header",  "");
+*/		req.setAttribute("content", "/Auction/InsertAuction.jsp");
+
+		// 프로젝트 경로를 얻어와 변수에 담음
 		String cp = req.getContextPath();
 		req.getServletContext().setAttribute("cp", cp);
-		System.out.println(cp);
-		//req.setAttribute("header",  "");
-		req.setAttribute("content", "/Auction/InsertAuction.jsp");
 		
+		// 
 		req.getRequestDispatcher("/index.jsp").forward(req, resp);	
 	}
 }
