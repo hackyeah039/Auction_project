f31d93dcd8e186a82731bdb3fc44db221dab00ae
admin <admin@DESKTOP-J54BCRR>
2020-05-17 00:46:24 +0900

    테스트 - footer 및 메인 추가

--- src/semi/controller/yhauction/HomeController.java
+++ src/semi/controller/yhauction/HomeController.java
@@ -13,20 +13,20 @@ public class HomeController extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-/*		// 홈페이지 호출시 기본적으로 보여질 페이지 설정.
+		// 홈페이지 호출시 기본적으로 보여질 페이지 설정.
 		String header = (String)req.getAttribute("header");
 		String content = (String)req.getAttribute("content");
 		String footer = (String)req.getAttribute("footer");
 		// 초기에는 호출된 값들이 없으므로 null 값일때 보여질 주소값 대입
 		if(header == null) header = "/header.jsp";
-		if(content == null) content = "/mainpage.jsp";
+		if(content == null) content = "/main.jsp";
 		if(footer == null) footer = "/footer.jsp";
-*/		
-/*
+
+		
 		//페이지 이동 전 페이지 주소에 대한 값을 담음
-		req.setAttribute("header",  "header");
-		req.setAttribute("footer",  "footer");
-*/		req.setAttribute("content", "/Auction/InsertAuction.jsp");
+		req.setAttribute("header",  header);
+		req.setAttribute("footer",  footer);
+		req.setAttribute("content", content);
 
 		// 프로젝트 경로를 얻어와 변수에 담음
 		String cp = req.getContextPath();
