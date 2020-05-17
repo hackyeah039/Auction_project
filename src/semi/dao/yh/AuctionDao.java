2575383eaf34af4807f724c78f05508c8abccc25
admin <admin@DESKTOP-J54BCRR>
2020-05-17 23:17:30 +0900

    0517 - DB 저장 메소드 추가 [신규일때 - seller, auction, ship, img] [기존판매자일때 - auction, ship, img]

--- src/semi/dao/yh/AuctionDao.java
+++ src/semi/dao/yh/AuctionDao.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 
 import jdbc.JdbcUtil;
 import semi.vo.yh.AuctionVo;
+import semi.vo.yh.SellerVo;
+import semi.vo.yh.ShipVo;
 
 public class AuctionDao {
 	private static AuctionDao instance = new AuctionDao();
@@ -80,6 +82,87 @@ public class AuctionDao {
 			return -1;
 		} finally {
 			JdbcUtil.close(con, pstmt, null);
+		}
+	}
+	public int InsertTables(int sel_number, SellerVo sevo, AuctionVo avo, ShipVo shvo, ArrayList<String> fList) {
+		Connection con = null;
+		PreparedStatement pstmtSeller = null;
+		PreparedStatement pstmtAuction = null;
+		PreparedStatement pstmtShip = null;
+		PreparedStatement pstmtImg = null;
+		try {
+			con = JdbcUtil.getConn();
+			con.setAutoCommit(false);
+			// 신규일때 -> seller, action
+			if(sel_number == 0 ) {
+				String sqlSeller = "insert into seller values(?,?,seq_seller_sel_number.nextval)";
+				pstmtSeller = con.prepareStatement(sqlSeller);
+				pstmtSeller.setLong(1, sevo.getAccount());
+				pstmtSeller.setInt(2, sevo.getM_num());
+				pstmtSeller.executeUpdate();
+				
+				
+				// 문자열이라고 미리 작은 따옴표로 감쌀 필요 없음, MM과 mm은 같은 문자열로 인식 되므로 분단위 mm을 mi로 수정
+				String sqlAuction = "insert into auction values(seq_auction_a_num.nextval,?,?,?,sysdate,to_date(?,'yyyy-MM-dd HH24:mi:ss'),to_date(?,'yyyy-MM-dd HH24:mi:ss'),0,?,0,seq_seller_sel_number.currval,?,?,?)";
+				pstmtAuction = con.prepareStatement(sqlAuction);			
+				pstmtAuction.setString(1, avo.getA_title());
+				pstmtAuction.setString(2, avo.getA_content());
+				pstmtAuction.setInt(3, avo.getA_condition());
+				pstmtAuction.setString(4, avo.getA_startdate());
+				pstmtAuction.setString(5, avo.getA_enddate());
+				pstmtAuction.setInt(6, avo.getC_num());
+				pstmtAuction.setInt(7, avo.getBidstatus());
+				pstmtAuction.setInt(8, avo.getA_startbid());
+				pstmtAuction.setInt(9, avo.getA_bidunit());
+				pstmtAuction.executeUpdate();
+			} else {
+				// 기존 등록자일때 
+				String sqlAuction = "insert into auction values(seq_auction_a_num.nextval,?,?,?,sysdate,to_date(?,'yyyy-MM-dd HH24:mi:ss'),to_date(?,'yyyy-MM-dd HH24:mi:ss'),0,?,0,?,?,?,?)";
+				pstmtAuction = con.prepareStatement(sqlAuction);			
+				pstmtAuction.setString(1, avo.getA_title());
+				pstmtAuction.setString(2, avo.getA_content());
+				pstmtAuction.setInt(3, avo.getA_condition());
+				pstmtAuction.setString(4, avo.getA_startdate());
+				pstmtAuction.setString(5, avo.getA_enddate());
+				pstmtAuction.setInt(6, avo.getC_num());
+				pstmtAuction.setInt(7, avo.getSel_number());
+				pstmtAuction.setInt(8, avo.getBidstatus());
+				pstmtAuction.setInt(9, avo.getA_startbid());
+				pstmtAuction.setInt(10, avo.getA_bidunit());
+				pstmtAuction.executeUpdate();
+			}
+			
+			String sqlShip = "insert into Ship values(seq_ship_s_num.nextval, ?, ?, seq_auction_a_num.currval)";
+			pstmtShip = con.prepareStatement(sqlShip);
+			pstmtShip.setString(1, shvo.getS_way());
+			pstmtShip.setInt(2, shvo.getS_price());
+			pstmtShip.executeUpdate();
+			
+			System.out.println(fList + "     asdae");
+
+			for(String path:fList) {
+				String sqlImg = "insert into img values(seq_auction_a_num.currval,?)";
+				System.out.println(path + "     111111");
+				pstmtImg = con.prepareStatement(sqlImg);
+				pstmtImg.setString(1, path);
+				pstmtImg.executeUpdate();
+			}
+			con.commit();
+			return 1;
+		} catch (SQLException se) {
+			se.printStackTrace();
+			try {
+				con.rollback();
+			} catch (SQLException se1) {
+				se1.printStackTrace();
+			}
+			return -1;
+		} finally {
+			JdbcUtil.close(pstmtImg);
+			JdbcUtil.close(pstmtShip);
+			JdbcUtil.close(pstmtAuction);
+			JdbcUtil.close(pstmtSeller);
+			JdbcUtil.close(con);
 		}
 	}
 }
